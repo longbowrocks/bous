625a081b776d21e01d2b3c452280e6f30cd58caf
Garrett Newman <garrett.s.newman@gmail.com>
2017-11-25 17:06:52 -0800

    main-er

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import logging, sys
 import random
 import pygame
 from pygame.locals import *
-from planet import Planet
 from keys import Keys
+from game import Game
 
 # TODO: create game screen
 # TODO:
@@ -30,9 +30,9 @@ def init_resources():
     pygame.mixer.quit()
     pygame.mixer.init(frequency=44100, buffer=0)
 
-    clock = pygame.time.Clock()
-
-    sys_font = pygame.font.Font("./8514oem.fon", 20)
+    # clock = pygame.time.Clock()
+    #
+    # sys_font = pygame.font.Font("./8514oem.fon", 20)
 
 def get_planets(count):
     planets = []
@@ -44,31 +44,37 @@ def get_player():
     return pygame.Rect
 
 def game_loop():
-  delta_t = clock.tick(60)
+    delta_t = clock.tick(60)
 
-  # key stuff
-  keys.update(pygame.event.get([KEYDOWN, KEYUP]))
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-  if keys.alt and keys.f4:
-    pygame.quit()
-    sys.exit()
-  if keys.f11_toggle and not (screen.get_flags() & pygame.FULLSCREEN):
-    pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
-  elif not keys.f11_toggle and screen.get_flags() & pygame.FULLSCREEN:
-    pygame.display.set_mode(RESOLUTION)
+    # key stuff
+    keys.update(pygame.event.get([KEYDOWN, KEYUP]))
+    for event in pygame.event.get():
+      if event.type == QUIT:
+          pygame.quit()
+          sys.exit()
+    if keys.alt and keys.f4:
+        pygame.quit()
+        sys.exit()
+    if keys.f11_toggle and not (screen.get_flags() & pygame.FULLSCREEN):
+        pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
+    elif not keys.f11_toggle and screen.get_flags() & pygame.FULLSCREEN:
+        pygame.display.set_mode(RESOLUTION)
 
-  # update stuff
-  if not keys.p_toggle:
-    runTick(delta_t)
+    # update stuff
+    if not keys.p_toggle:
+        runTick(delta_t)
 
-  # prepare next frame for display
-  drawGame(screen)
+    # prepare next frame for display
+    drawGame(screen)
 
-  pygame.display.flip()
+    pygame.display.flip()
 
 if __name__ == '__main__':
+    # Init stuff
+    init_resources()
+
+    game = Game(keys)
+
+    # Loop stuff
     while True:
-      gameLoop()
+        game.loop()
