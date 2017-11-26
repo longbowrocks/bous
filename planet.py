d19edd697f527c058e825f19d78b5116788b5cae
cmercer <longbowmail@yahoo.com>
2017-11-25 21:27:01 -0800

    When worlds... er, don't collide.

--- planet.py
+++ planet.py
@@ -1,7 +1,9 @@
 from gameColor import GameColor
 import random, math
+import logging
 import pygame
-from constants import RESOLUTION, MIN_PLANET_SIZE, MAX_PLANET_SIZE
+from pointUtils import dist_squared
+from constants import RESOLUTION, MIN_PLANET_SIZE, MAX_PLANET_SIZE, PLAYER_SIZE
 
 
 class Planet():
@@ -16,11 +18,22 @@ class Planet():
         overlaps = True
         num_tries = 0
         while overlaps and num_tries < 100:
+            num_tries += 1
             pos = Planet.get_random_legal_position()
             radius = random.randint(MIN_PLANET_SIZE, MAX_PLANET_SIZE)
-            num_tries += 1
+            if pos[0] + radius + PLAYER_SIZE > RESOLUTION[0] or\
+                pos[0] - radius - PLAYER_SIZE < 0:
+                continue # planet overlaps with screen X bounds
+            elif pos[1] + radius + PLAYER_SIZE > RESOLUTION[1] or\
+                pos[1] - radius - PLAYER_SIZE < 0:
+                continue # planet overlaps with screen Y bounds
             overlaps = False
+            for planet in existing_planets:
+                if dist_squared(pos, planet.pos) < (radius + planet.radius)**2:
+                    overlaps = True
+                    break
         if num_tries >= 100:
+            logging.error("Tried to place a planet in an overpopulated system.")
             return Planet(MIN_PLANET_SIZE, (0, 0), GameColor.White)
         color = GameColor.Red
         return Planet(radius, pos, color)
