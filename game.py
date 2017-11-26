7ec20bf1bd99b7d1026f58ba415e5a274068d1a0
Garrett Newman <garrett.s.newman@gmail.com>
2017-11-25 17:46:17 -0800

    sorry

--- game.py
+++ game.py
@@ -17,18 +17,22 @@ class Game:
 
     def reset(self):
         # init
-        self.planets = init_planets()
-        self.players = init_players()
+        self.planets = self.init_planets(5)
+        self.players = self.init_players(2)
 
-        self.player = players[0]
+        self.player = self.players[0]
 
     def init_planets(self, count):
-        return [Planet.create_non_overlapping(planets) for idx in count]
+        planets = []
+        for idx in range(count):
+            planets.append(Planet.create_non_overlapping(planets))
+        return planets
 
     def init_players(self, count):
-        return [pygame.Rect for idx in count]
+        return [pygame.Rect for idx in range(count)]
 
     def loop(self):
-        #
+        pass
 
     def draw(self, screen):
+        pass
